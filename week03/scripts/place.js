933e4d7985ba34a6944b4b36c6ac1d3a7319b485
Jonathan Nicolas Pacheco Mera <jonypachecomera@gmail.com>
2026-07-20 23:39:33 -0300

    Complete place assigment.

---
+++ week03/scripts/place.js
@@ -0,0 +1,25 @@
+const currentYear = document.querySelector("#currentyear");
+const lastModified = document.querySelector("#lastModified");
+
+currentYear.textContent = new Date().getFullYear();
+lastModified.textContent = `Last Modification: ${document.lastModified}`;
+
+const temperature = 28;
+const windSpeed = 10;
+
+function calculateWindChill(temp, wind) {
+    return (
+        13.12 +
+        0.6215 * temp -
+        11.37 * Math.pow(wind, 0.16) +
+        0.3965 * temp * Math.pow(wind, 0.16)
+    ).toFixed(1);
+}
+
+const windChill = document.querySelector("#windchill");
+
+if (temperature <= 10 && windSpeed > 4.8) {
+    windChill.textContent = `${calculateWindChill(temperature, windSpeed)} °C`;
+} else {
+    windChill.textContent = "N/A";
+}
